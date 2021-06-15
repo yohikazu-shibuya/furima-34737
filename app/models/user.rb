a3ea8c5a2b05106a0fecfac69ebb85f0e2d798b0
yohikazu-shibuya <ys0979@gmail.com>
2021-06-15 22:56:45 +0900

    新規登録画面エラーメッセージ

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :encrypted_password, presence: true, presence: {message: 'is invalid. Include both letters and numbers'}
+         validates :nickname, presence: true, presence: {message: ' cant be blank'}
+         validates :birthday, presence: true, presence: {message: ' date cant be blank'}
+         validates :last_name, presence: true, presence: {message: ' cant be black'}
+         validates :last_name, presence: true, presence: {message: ' is invalid. Input full-width characters'}
+         validates :first_name, presence: true, presence: {message: ' cant be black'}
+         validates :first_name, presence: true, presence: {message: ' is invalid. Input full-width characters'}
+         validates :last_name_kana, presence: true, presence: {message: ' cant be black'}
+         validates :last_name_kana, presence: true, presence: {message: ' is invalid. Input full-width characters'}
+         validates :first_name_kana, presence: true, presence: {message: ' cant be black'}
+         validates :first_name_kana, presence: true, presence: {message: ' is invalid. Input full-width characters'}
 end

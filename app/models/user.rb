9d2d8010b5591665c6de79885fd95c46fb84ebe6
yohikazu-shibuya <ys0979@gmail.com>
2021-06-17 22:22:40 +0900

    userモデルテスト単体コード完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,13 @@ class User < ApplicationRecord
   
          validates :nickname, presence: true, presence: {message: "can't be blank"}
          validates :encrypted_password, presence: true, presence: {message: 'is invalid. Include both letters and numbers'}
-         validates :birthday, presence: true, presence: {message: ' date cant be blank'}
-         validates :last_name, presence: true, presence: {message: ' cant be black'}
-         validates :last_name, presence: true, presence: {message: ' is invalid. Input full-width characters'}
-         validates :first_name, presence: true, presence: {message: ' cant be black'}
-         validates :first_name, presence: true, presence: {message: ' is invalid. Input full-width characters'}
-         validates :last_name_kana, presence: true, presence: {message: ' cant be black'}
-         validates :last_name_kana, presence: true, presence: {message: ' is invalid. Input full-width characters'}
-         validates :first_name_kana, presence: true, presence: {message: ' cant be black'}
-         validates :first_name_kana, presence: true, presence: {message: ' is invalid. Input full-width characters'}
+         validates :birthday, presence: true, presence: {message: "date can't be blank"}
+         validates :last_name, presence: true, presence: {message: "can't be blank"}
+         validates :last_name, presence: true, presence: {message: 'is invalid. Input full-width characters'}
+         validates :first_name, presence: true, presence: {message: "can't be blank"}
+         validates :first_name, presence: true, presence: {message: 'is invalid. Input full-width characters'}
+         validates :last_name_kana, presence: true, presence: {message: "can't be blank"}
+         validates :last_name_kana, presence: true, presence: {message: 'is invalid. Input full-width characters'}
+         validates :first_name_kana, presence: true, presence: {message: "can't be blank"}
+         validates :first_name_kana, presence: true, presence: {message: 'is invalid. Input full-width characters'}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,31 +6,70 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
-    it '全ての情報が存在すれば登録できる' do
-      expect(@user).to be_valid
+    context '新規登録できるとき' do
+      it '全ての情報が存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+      it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
+        @user.password = "000000"
+        @user.password_confirmation = "000000"
+        expect(@user).to be_valid
+      end
     end
-    it 'nicknameが空では登録できない' do
-      @user.nickname = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Nickname can't be blank")
-    end
-    it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
-    end
-    it 'emailが空では登録できない' do
-    end
-    it 'passwordが空では登録できない' do
-    end
-    it 'passwordが存在してもpassword_confirmationが空では登録できない' do
-    end
-    it '名前の漢字が空では登録できない' do
-    end
-    it '名前の読みカナが空では登録できない' do
-    end
-    it '生年月日が空では登録ができない' do
-    end
-    it '重複したemailが存在する場合登録できない' do
-    end
-    it 'passwordが5文字以下では登録できない' do
+    context '新規登録できないとき' do
+      it 'nicknameが空では登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+        it 'emailが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+        @user.password = "000000"
+        @user.password_confirmation = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it '名前の漢字が空では登録できない' do
+        @user.last_name = ''
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
+        expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+      it '名前の読みカナが空では登録できない' do
+        @user.last_name_kana = ''
+        @user.first_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
+      end
+      it '生年月日が空では登録ができない' do
+        @user.birthday = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday date can't be blank")
+      end
+      it '重複したemailが存在する場合登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = '00000'
+        @user.password_confirmation = '00000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
     end
   end
 end

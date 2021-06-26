1f1897897043148faa1f7ce1ec3a262a230dee92
yohikazu-shibuya <ys0979@gmail.com>
2021-06-26 18:14:46 +0900

    ルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,19 @@
 class ItemsController < ApplicationController
+  
   def index
     @items = Item.all
   end
 
   def new
-    @item = Iweet.new
+    @item = Item.new
   end
 
-  # def create
-  #   Items.create(item_params)
-  # end
+  def create
+    @item = Item.create
+  end
   
-  # private
-  # def item_params
-  #   parms.require(:item).permit()
-  # end
+  private
+  def item_params
+    params.require(:item).permit(:product, :product_description, :category_id, :product_detail_id, :ship_base_id, :prefecture_id, :ship_date_id, :price)
+  end
 end

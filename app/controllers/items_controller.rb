ce4c68c82cc59044d1b63fccbf6438bede1f60cb
yohikazu-shibuya <ys0979@gmail.com>
2021-06-26 21:13:32 +0900

    active_hash実装完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   
   def index
     @items = Item.all
+    @items = Item.order("created_at DESC")
   end
 
   def new
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :product_detail
+  belongs_to :ship_base
+  belongs_to :prefecture
+  belongs_to :ship_date
+
+  validates :genre_id, numericality: { other_than: 1 }
+  validates :product_detail_id, numericality: { other_than: 1 }
+  validates :ship_base_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :ship_date_id, numericality: { other_than: 1 }
 end

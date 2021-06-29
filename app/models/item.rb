d7aeed3ac05e420badc6e6135f55c252b26c9e80
yohikazu-shibuya <ys0979@gmail.com>
2021-06-29 20:20:37 +0900

    model_validetes設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,9 +10,11 @@ class Item < ApplicationRecord
 
   has_one_attached :image
   
-  validates :product, presence: true
-  validates :product_description, presence: true
-  validates :price, presence: true
+  validates :product, presence: true, length: { maximum: 40}
+  validates :product_description, presence: true, length: { maximum: 1000}
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300,
+  less_than_or_equal_to: 9999999 }
+
 
   validates :category_id, numericality: { other_than: 1 }
   validates :product_detail_id, numericality: { other_than: 1 }

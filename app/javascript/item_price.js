c96124dd2574dddfb46b4db2f7adfddef9712d3a
yohikazu-shibuya <ys0979@gmail.com>
2021-07-03 13:06:55 +0900

    java販売価格

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,10 @@
-window.addEventListener('load', () => {
-  console.log("OKOK");
+window.addEventListener('DOMContentLoaded', () => {
+   const priceInput = document.getElementById("item-price");
+   priceInput.addEventListener("input", () => {
+      const addTaxDom = document.getElementById("add-tax-price");
+      addTaxDom.innerHTML = Math.floor(priceInput.value / 10 ).toLocaleString();
+      const salesCommission = document.getElementById("profit");
+      salesCommission.innerHTML = (priceInput.value - Math.floor(priceInput.value /10)).toLocaleString();
+
+   })
 });

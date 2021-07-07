21de0a4feddb3c0511a8d2d34f9a3f3304590e5d
yohikazu-shibuya <ys0979@gmail.com>
2021-07-07 17:45:22 +0900

    edit編集まで

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,10 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :product, :product_description, :category_id, :product_detail_id, :ship_base_id, :prefecture_id, :ship_date_id, :price).merge(user_id: current_user.id)

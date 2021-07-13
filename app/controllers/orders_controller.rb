44e40664f67f054d8b456d6b1695199c452dedfe
yohikazu-shibuya <ys0979@gmail.com>
2021-07-13 13:09:26 +0900

    住所_item情報入力

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,29 @@ class OrdersController < ApplicationController
 
   def index
     @orders
+    @item = Item.find(params[:item_id])
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+    @order = Order.create(order_params)
+    Address.create(address_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def order_params
+    params.permit(:item_id).merge(user_id: current_user.id)
+  end
+
+  def address_params
+    params.permit(:post_code, :prefecture_id, :city, :block, :building, :phone_number).merge(order_id: @order.id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items do
-  resources :orders, only: [:index, :create]
+  resources :orders, only: [:index, :create, :show]
 end
 resources :users, only: :show
 end
